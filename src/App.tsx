a620653513e9e8924a2f1ac5d7bc1468b0a37117
Filip Mrdjanovic <mrkifilip@gmail.com>
2022-12-27 20:47:13 +0100

    Added routing for about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,34 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Wrapper from './components/wrapper/Wrapper';
+import { Routes, Route, Link } from "react-router-dom";
+import Page from './components/page/Page';
+
 
 function App() {
- 
   return (
-    <div className="App" style={{overflow: 'hidden'}}>
-      <Wrapper />
-    </div>
+    // <div className="App">      
+               
+    //   <Routes>
+    //     <Route path='/' element={<Wrapper/>} />
+    //     <Route path='/about' element={<Page/>} />
+    //     <Route path='/work' element={<Page/>} />
+    //   </Routes>
+    // </div>
+    
+    // <>
+    //   <nav>
+    //     <ul>
+    //       <li><Link to="/">Home</Link></li>
+    //       <li><Link to="/books">Books</Link></li>
+    //     </ul>
+    //   </nav>
+    // <Routes>
+    //   <Route path="/" element={<Wrapper />} />
+    //   <Route path="/page" element={<Page />} />
+    // </Routes>
+    // </>
+    <Page />
   );
 }
 
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Wrapper from '../wrapper/Wrapper';
+import AboutPage from './about/AboutPage';
 
 export default function Page() {
   return (
-    <div>Page</div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Wrapper />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
